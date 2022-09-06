1cbd7a7ead1e1b0be70b628d10afeaa458944111
HiraPee <yuta.hira-p0119@docomo.ne.jp>
2022-09-06 15:30:52 +0900

    Add

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AboutPage from './components/AboutPage';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <Router basename={"https://HiraPee.github.io/portfolio"}>
+    <Router basename={"https://hirapee.github.io/portfolio"}>
     <div className="App">
     <Header />
       <Routes >
